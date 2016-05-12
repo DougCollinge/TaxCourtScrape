1422b3fd37e50002adcb9d5020a2f4a588ce25f1
Doug Collinge <doug.collinge@gmail.com>
2016-05-12 16:33:37 -0700

    Testing reading back the CSV file.

--- Tester.py
+++ Tester.py
@@ -1,5 +1,5 @@
-from TCCScraper import TCCScraper
-tcc = TCCScraper()
+# from TCCScraper import TCCScraper
+# tcc = TCCScraper()
 # docket = tcc.get_hearings()
 
 # for hearing in docket :
@@ -9,7 +9,22 @@ tcc = TCCScraper()
 # data = tcc.get_hearing_data("2012-4043(IT)G")
 # print(data)
 
-def progress(max,count) :
-    print( 'Progress: {}/{} '.format(count,max) )
+# def progress(max,count) :
+#     print( 'Progress: {}/{} '.format(count,max) )
+#
+# tcc.get_hearings_table(progress,limitcount=2)
 
-tcc.get_hearings_table(progress,limitcount=2)
+import csv
+oldtable = []
+with open('TCCHearings2016-05-11.csv') as file :
+    rdr = csv.DictReader(file)
+    for row in rdr :
+        oldtable.append(row)
+# print( oldtable[:4] )
+
+byfileno = {}
+for row in oldtable :
+    byfileno[row["File No"]] = row
+
+print(byfileno["2015-3471(IT)APP"])
+print( list(byfileno.keys()) )
